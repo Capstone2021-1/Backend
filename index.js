59eb94272f6600973c01a2add2731577ff08c423
tbqlsmm <qldtn8@naver.com>
2021-05-03 13:03:12 +0000

    충전소 검색 API 작성 완료

--- index.js
+++ index.js
@@ -206,7 +206,46 @@ app.get('/getChargingFee', (req, res) => {
 			})
 		}
 	})
-})
+});
+
+var client_id = 'JLVMtPNtATW6dqr4sGCS';
+var client_secret = 'ApyaiBWcdu';
+app.get('/search/charging_station', function (req, res) {
+	var sql = `SELECT id, name, lng, lat FROM charging_station WHERE name LIKE '%${req.query.query}%' OR address LIKE '%${req.query.query}%';`;
+	var totalResult = new Array();
+	db.query(sql, (err, result) => {
+		if(err) res.json(err);
+		for(var i = 0; i < result.length; i++){
+			var search = new Object();
+			search.iOt = result[i].id; search.nOa = result[i].name;
+			search.lngOx = result[i].lng; search.latOy = result[i].lat; search.isChSt = 1;
+			totalResult.push(search);
+		}
+	});
+
+	var api_url = 'https://openapi.naver.com/v1/search/local?query=' + encodeURI(req.query.query) + '&display=5';
+	var request = require('request');
+	var options = {
+       		url: api_url,
+       		headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    	};
+   	request.get(options, function (error, response, body) {
+     	if (!error && response.statusCode == 200) {
+		var temp = JSON.parse(body).items;
+		for(var i = 0; i < temp.length; i++) {
+			var search = new Object();
+			search.iOt = temp[i].title; search.nOa = temp[i].address;
+			search.lngOx = temp[i].mapx; search.latOy = temp[i].mapy; search.isChSt = 0;
+			totalResult.push(search);
+		}
+		res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+		res.end(JSON.stringify(totalResult));
+     	} else {
+       		res.status(response.statusCode).end();
+       		console.log('error = ' + response.statusCode);
+	}
+	});
+});
 
 http.createServer(app).listen(80, () => {
 	console.log('http server running on port 80');
